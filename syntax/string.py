38da84f9dda52f58dd5a599f805b71669a2188f7
goodchuck <y502100@naver.com>
2021-05-14 17:41:53 +0900

    21/05/14 생활코딩 문법 공부 5.1

---
+++ syntax/string.py
@@ -0,0 +1,22 @@
+print('Hello world') #이걸 좀더 많이쓴다.
+print("Hello world") #둘다 같음
+print("Hell'o' world") #이렇게하면 작은따옴표를 표시할수있다
+
+#escape
+print("Hell'o' \"w\"orld") # 파이썬은 \가나오면 직후에 오는 문자하나를 문자로만듬 ex) \" escape
+
+#newline
+print('H')
+print('e')
+print('l')
+print('l')
+print('o')
+print('H\ne\nl\nl\no')
+#docstring
+print('''
+H
+e
+l
+l
+o
+''')
